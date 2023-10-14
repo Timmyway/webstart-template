f27c175b0a7836ecdd4154ab2e1483a91a90deb8
tim <timmyway1990@gmail.com>
2023-10-15 00:18:32 +0300

    chore: Add .vite folder to gitignore
    - Also change active item color (navbar).

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,8 +8,8 @@
             <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                    <a href="/" class="<?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-                    <a href="/about" class="<?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>                
+                    <a href="/" class="<?= urlIs('/') ? 'bg-yellow-400 text-dark' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
+                    <a href="/about" class="<?= urlIs('/about') ? 'bg-yellow-400 text-dark' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About</a>
                 </div>
             </div>
             </div>
@@ -74,8 +74,8 @@
         <div class="md:hidden" id="mobile-menu">
         <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-            <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>                    
+            <a href="/" class="<?= urlIs('/') ? 'bg-yellow-400 text-dark' : 'text-gray-300' ?> block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
+            <a href="/" class="<?= urlIs('/about') ? 'bg-yellow-400 text-dark' : 'text-gray-300' ?> block rounded-md px-3 py-2 text-base font-medium">About</a>                    
         </div>
         <div class="border-t border-gray-700 pb-3 pt-4">
             <div class="flex items-center px-5">
